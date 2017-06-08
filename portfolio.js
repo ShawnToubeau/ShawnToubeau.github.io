b83270ec02c595fe0dab0e6e9ed957424c050342
Shawn Toubeau <shawntoubeau@gmail.com>
2017-06-08 02:12:46 -0400

    Fingers Crossed
    Hoping this works!

---
+++ portfolio.js
@@ -0,0 +1,29 @@
+$ (document).ready(function() {
+  $ ('a').on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+$(".navbar-collapse a").click(function() {
+  $(".navbar-collapse").collapse("hide");
+});
+
+$(document).click(function(e) {
+  if (!$(e.target).is("a") || $(e.target).is(".navbar-brand")) {
+    $(".collapse").collapse("hide");
+  }
+});
