5bb9be751fd799cec4ad6b81ba7817def697e315
Shawn Toubeau <shawntoubeau@gmail.com>
2017-06-14 18:00:30 -0400

    Bug Fix

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-  $("a").on("click", function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
+$(document).ready(function () {
+  $('a').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault()
 
-      var hash = this.hash;
+      var hash = this.hash
 
-      $("html, body").animate(
+      $('html, body').animate(
         {
           scrollTop: $(hash).offset().top
         },
         800,
-        function() {
-          window.location.haHsh = hash;
+        function () {
+          window.location.haHsh = hash
         }
-      );
+      )
     }
-  });
-});
+  })
+})
 
-$(".navbar-collapse a").click(function() {
-  $(".navbar-collapse").collapse("hide");
-});
+$('.navbar-collapse a').click(function () {
+  $('.navbar-collapse').collapse('hide')
+})
 
-$(document).click(function(e) {
-  if (!$(e.target).is("a") || $(e.target).is(".navbar-brand")) {
-    $(".collapse").collapse("hide");
+$(document).click(function (e) {
+  if (!$(e.target).is('a') || $(e.target).is('.navbar-brand')) {
+    $('.collapse').collapse('hide')
   }
-});
+})
